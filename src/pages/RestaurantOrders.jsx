8469503ecb3b7d6bfb076f62b2803c4cc0afde75
Raphael <raphaelolagoke70@gmail.com>
2025-07-11 15:33:30 +0100

    Current Version

--- src/pages/RestaurantOrders.jsx
+++ src/pages/RestaurantOrders.jsx
@@ -51,7 +51,7 @@ const RestaurantOrders = () => {
         const token = localStorage.getItem('token');
 
         try {
-            const response = await fetch(`http://localhost:8080/api/v1/invoice/download?ref=${ref}`, {
+            const response = await fetch(`${BASE_URL}/invoice/download?ref=${ref}`, {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${token}`,
